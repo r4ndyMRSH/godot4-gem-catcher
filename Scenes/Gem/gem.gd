660391b770dba551f83abc922050135eb2064a54
r4ndyMRSH <pvlnzr@gmail.com>
2025-09-24 13:20:16 +0300

    added initial files

---
+++ Scenes/Gem/gem.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+const SPEED: float = 100.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.y += SPEED * delta
+	if position.y > get_viewport_rect().end.y:
+		print("Gems falls off")
+		set_process(false)
+		# Queues this node to be deleted at the end of the current frame. 
+		# When deleted, all of its children are deleted as well, 
+		# and all references to the node and its children become invalid
+		queue_free()
---
+++ Scenes/Paddle/paddle.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+const PADDLE_SPEED: float = 500.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_left") == true:
+		position.x = clampf(position.x - PADDLE_SPEED * delta,52.0,get_viewport_rect().end.x - 52.0)
+	if Input.is_action_pressed("move_right") == true:
+		position.x = clampf(position.x + PADDLE_SPEED * delta,52.0,get_viewport_rect().end.x - 52.0)
