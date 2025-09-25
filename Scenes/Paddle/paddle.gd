874db1540a1a478f09faf96a151ab39d54ec6072
r4ndyMRSH <pvlnzr@gmail.com>
2025-09-25 11:08:25 +0300

    Using get_axis method

--- Scenes/Paddle/paddle.gd
+++ Scenes/Paddle/paddle.gd
@@ -9,7 +9,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("move_left") == true:
-		position.x = clampf(position.x - PADDLE_SPEED * delta,52.0,get_viewport_rect().end.x - 52.0)
-	if Input.is_action_pressed("move_right") == true:
-		position.x = clampf(position.x + PADDLE_SPEED * delta,52.0,get_viewport_rect().end.x - 52.0)
+	#if Input.is_action_pressed("move_left") == true:
+	#	position.x = clampf(position.x - PADDLE_SPEED * delta,52.0,get_viewport_rect().end.x - 52.0)
+	#if Input.is_action_pressed("move_right") == true:
+	#	position.x = clampf(position.x + PADDLE_SPEED * delta,52.0,get_viewport_rect().end.x - 52.0)
+	var movement: float = Input.get_axis("move_left","move_right")
+	position.x += PADDLE_SPEED * delta * movement
+	position.x = clampf(position.x,
+	get_viewport_rect().position.x + 52.0,
+	get_viewport_rect().end.x - 52.0)
